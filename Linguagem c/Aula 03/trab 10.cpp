7a8d270ae5d2625b8f4641449af4c1241c1148c4
gustavojose136 <gjose2980@gmail.com>
2022-04-04 13:28:08 -0300

    a

--- Linguagem c/Aula 03/trab 10.cpp
+++ Linguagem c/Aula 03/trab 10.cpp
@@ -3,14 +3,15 @@
 #include <math.h>
 
 int main (void){
-	float a, b, d, n1;
+	float a, b, c, d, n1;
 	d=25;
 	printf("Digite quantas horas voce trabalhou este mes: \n");
 	scanf("%f", &n1);
 	fflush(stdin);
 	a= d*n1;
-	b*=1-0,08; 
-	printf("%.1f", b);
+	b= (a*0,8)/100;
+	c= a-b;
+	printf("%.1f", c);
 
 
 }
